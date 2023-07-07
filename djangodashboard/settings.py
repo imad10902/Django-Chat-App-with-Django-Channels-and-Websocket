99f7a71dd9ce3b2d237cb356d324bd57b8796268
Imad Bin Asad <105483337+imad10902@users.noreply.github.com>
2023-07-08 03:42:28 +0530

    Update settings.py

--- djangodashboard/settings.py
+++ djangodashboard/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-@%mrk8d%++7%*qh8u9fm%d*f#0yfv)5odp_^yv93uf021g(bjy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -125,6 +128,5 @@ AUTH_USER_MODEL = 'base.CustomUser'
 MEDIA_URL = '/media/'  
   
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
